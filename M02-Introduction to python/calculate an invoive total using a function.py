077166e19dcfd8f4dde59d67fcf083870978f55b
saranyatinku <bsaranya303@gmail.com>
2026-08-15 15:52:20 +0530

    calculate an invoice total using a function

---
+++ M02-Introduction to python/calculate an invoive total using a function.py
@@ -0,0 +1,8 @@
+def display_invoice_total(price, quantity):
+    # Write your code here
+    total = price * quantity
+    print("Total:", total)
+
+price = int(input())
+quantity = int(input())
+display_invoice_total(price, quantity)
