454f6bc1d936b251df12796924ac55cfba3f683c
saranyatinku <bsaranya303@gmail.com>
2026-08-15 14:27:18 +0530

    convert student registration values

---
+++ M02-Introduction to python/convert student registration values.py
@@ -0,0 +1,15 @@
+age_text = "21"
+course_fee_text = "499.50"
+attempts_text = 3
+
+# Display the converted values and their data types
+age_text = int(age_text)
+course_fee_text = float(course_fee_text)
+attempts_text = str(attempts_text)
+
+print(age_text)
+print(type(age_text))
+print(course_fee_text)
+print(type(course_fee_text))
+print(attempts_text)
+print(type(attempts_text))
