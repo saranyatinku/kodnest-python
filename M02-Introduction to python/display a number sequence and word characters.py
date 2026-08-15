82e503aa56c7a0e3a14c2e66e330ba9c9b22c482
saranyatinku <bsaranya303@gmail.com>
2026-08-15 14:56:47 +0530

    display a number sequence and word characters

---
+++ M02-Introduction to python/display a number sequence and word characters.py
@@ -0,0 +1,13 @@
+# Read the number and word
+N = int(input())
+word = input()
+
+# Print the number sequence
+print("Numbers:")
+for i in range(1, N+1):
+    print(i)
+
+# Print the characters
+print("Characters:")
+for char in word:
+    print(char)
